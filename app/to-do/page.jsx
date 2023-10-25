bd5e6000b245d19c642acd19f2b5f30574f2dabf
Aiste Pilvinyte Wognsen <aiste.pilvinyte@gmail.com>
2023-10-25 12:41:41 +0200

    added some ideas for what to do next

--- app/to-do/page.jsx
+++ app/to-do/page.jsx
@@ -6,6 +6,14 @@ export const metadata = {
 }
 export default function ToDo() {
     return (
-        <div>To-to something</div>
+        <div>
+            <div>To-to something. What to do, what to do? Ideas?</div>
+            <ul className="list-disc pl-8 py-4">
+                <li>Fetch the data from GoodReads and think of something interesting to show</li>
+                <li>Make a little interactive to do with firebase</li>
+                <li>Add a cv entries? LinkedIn already does the good job...</li>
+                <li>Take a look at Tailwind and what can it do for me?</li>
+            </ul>
+        </div>
     )
 }
