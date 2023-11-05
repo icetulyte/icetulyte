50f36420ee9236c5e11641e424cd3df4096c842b
Aiste Pilvinyte Wognsen <aiste.pilvinyte@gmail.com>
2023-11-05 10:12:16 +0100

    adding a link to main page and using an asset

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <main>
-      The main part
+      <Image
+        src="/images/me.jpg"
+        height={144}
+        width={144}
+        alt="This is me!"
+      />
     </main>
   )
 }
--- components/header.jsx
+++ components/header.jsx
@@ -5,6 +5,7 @@ export default function Header() {
         <header className="ml-8 py-8">
             <h1 className="text-3xl mb-8">Aistė Pilvinytė Wognsen - Frontend developer</h1>
             <nav className="flex flex-row">
+                <Link className="pr-8" href="/">About me</Link>
                 <Link className="pr-8" href="/blog">Blog</Link>
                 <Link className="pr-8" href="/to-do">To-do</Link>
                 <Link className="pr-8" href="/books">Books</Link>
