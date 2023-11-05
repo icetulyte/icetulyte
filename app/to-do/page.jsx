f599e24c0fda83759eba4fe3367455ad47f5addc
Aiste Pilvinyte Wognsen <aiste.pilvinyte@gmail.com>
2023-11-05 09:49:31 +0100

    to-do extracted to it's own component

--- app/to-do/page.jsx
+++ app/to-do/page.jsx
@@ -1,19 +1,16 @@
+import ToDo from './to-do'
+
 export const metadata = {
     title: 'To do',
     openGraph: {
       title: 'To do',
     },
 }
-export default function ToDo() {
+export default function ToDoPage() {
     return (
         <div>
             <div>To-to something. What to do, what to do? Ideas?</div>
-            <ul className="list-disc pl-8 py-4">
-                <li>Fetch the data from GoodReads and think of something interesting to show</li>
-                <li>Make a little interactive to do with firebase</li>
-                <li>Add a cv entries? LinkedIn already does the good job...</li>
-                <li>Take a look at Tailwind and what can it do for me?</li>
-            </ul>
+            <ToDo />
         </div>
     )
 }
---
+++ app/to-do/to-do.jsx
@@ -0,0 +1,12 @@
+export default function ToDo() {
+    return (
+        <div>
+            <ul className=" pl-8 py-4">
+                <li>Fetch the data from some API and think of something interesting to show</li>
+                <li>Make a little interactive to do with firebase</li>
+                <li>Add a cv entries? LinkedIn already does the good job...</li>
+                <li>Take a look at Tailwind and what can it do for me?</li>
+            </ul>
+        </div>
+    );
+}
