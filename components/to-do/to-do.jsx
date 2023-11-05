98fbd7f0aece518448b593ebe95f93527ca26b09
Aiste Pilvinyte Wognsen <aiste.pilvinyte@gmail.com>
2023-11-05 11:42:22 +0100

    added clsx for conditional styling

--- components/to-do/to-do.jsx
+++ components/to-do/to-do.jsx
@@ -1,11 +1,15 @@
+import { clsx } from 'clsx'
+
 import styles from './to-do.module.css'
 
 export default function ToDo() {
     return (
         <div className={styles.container}>
             <ul className="pl-8 py-4">
-                <li>Fetch the data from some API and think of something interesting to show</li>
-                <li>Make a little interactive to do with firebase</li>
+                <li className={clsx({
+                    [styles.success]: true,
+                })}>Fetch the data from some API and think of something interesting to show</li>
+                <li className={clsx({[styles.error]: true})}>Make a little interactive to do with firebase</li>
                 <li>Add a cv entries? LinkedIn already does the good job...</li>
                 <li>Take a look at Tailwind and what can it do for me?</li>
             </ul>
